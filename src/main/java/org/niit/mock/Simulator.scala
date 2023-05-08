c960b8086189e0d076dd3f6ecabfe833d5f9d405
GakshLimei <1260671954@qq.com>
2023-05-08 22:34:30 +0800

    模拟数据

--- src/main/java/org/niit/mock/Simulator.scala
+++ src/main/java/org/niit/mock/Simulator.scala
@@ -96,8 +96,8 @@ object Simulator {
 
     // 转化为毫秒值
     val randomTS = new Timestamp(start + (random.nextDouble() * (end - start)).toLong).getTime
-    val timestamp = new Timestamp(randomTS) //将当前系统时间的毫秒值转化为Timestamp对象，表示一个SQL TIMESTAMP类型的时间。
-    val orderTime = sdf.format(new Date(randomTS)) //将当前系统时间的毫秒值转换为Date对象，并将其格式化为指定格式的日期字符串，格式化的规则由变量sdf决定。其中，sdf代表SimpleDateFormat对象，是Java中常用的日期格式化类，用于将时间格式化为指定的格式字符串
+    val timestamp = new Timestamp(randomTS) //将随机生成的时间的的毫秒值转化为Timestamp对象，表示一个SQL TIMESTAMP类型的时间。
+    val orderTime = sdf.format(new Date(randomTS)) //将随机生成的时间的的毫秒值转换为Date对象，并将其格式化为指定格式的日期字符串，格式化的规则由变量sdf决定。其中，sdf代表SimpleDateFormat对象，是Java中常用的日期格式化类，用于将时间格式化为指定的格式字符串
 
 
     Orders(userID, appIDRandom, cityIDRandom, restaurantIDRandom, food_categoryIDRandom, ordersRandom, ordersScoreRandom, orderTime, timestamp)
