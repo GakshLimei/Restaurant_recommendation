9fc4ca3ad495ca35172b450c14a6887cdb5b1234
GakshLimei <1260671954@qq.com>
2023-05-23 22:53:37 +0800

    、

--- src/main/java/org/niit/service/BatchDataService.scala
+++ src/main/java/org/niit/service/BatchDataService.scala
@@ -18,6 +18,7 @@ class BatchDataService {
   import org.apache.spark.sql.functions._
   import spark.implicits._
 
+
   def dataAnalysis(): Unit = {
     //离线分析，对历史数据进行分析历史数据一般会存在数据库中（MySQL/HBase）
     //1.连接数据库
@@ -25,65 +26,90 @@ class BatchDataService {
     val takeawayDao = new BatchDataDao
     val allInfoDS = takeawayDao.getTakeawayData()
     //需求一：
-//    hotrestaurantCountTop50(allInfoDS)
+//    hotCityCountTop10(allInfoDS)
     //需求二：
-    hotrestaurantRecommendTop20(allInfoDS)
+    hotfoodcategoryRecommendTop10(allInfoDS)
+    //需求三：
+//    hotfoodcategoryTop10(allInfoDS)
+    //需求四：
+//    hotrestaurantTop10(allInfoDS)
   }
 
 
-  def hotrestaurantCountTop50(allInfoDS: Dataset[OrderWithRecommendations]): Unit = {
+
+
+  def hotCityCountTop10(allInfoDS: Dataset[OrderWithRecommendations]): Unit = {
     //2.1统计前50道热点题 ----->>在数据库中，即使相同的题目，也是分布在不同行中的
     //张三  题目1  数学
     //李四  题目1  数学  =>题目1  2
     val hotTop50 = allInfoDS.groupBy("order_id")
       .agg(count("*") as "hotCount")
       .orderBy('hotCount.desc)
-      .limit(50)
+//      .limit(10)
     //2.2将hotTop50和allInfoDS进行关联，得到热点题对应的题目 dropDuplicates:去重
     val joinDF = hotTop50.join(allInfoDS.dropDuplicates("order_id"), "order_id")
     //2.3按学科分组聚合各个学科包含热点题的数量
     val res = joinDF.groupBy("city_id")
       .agg(count("*") as "hotCount")
       .orderBy('hotCount.desc)
+    println("--------统计推荐所属的热门城市Top10--------")
     res.show()
   }
 
-  //需求二： 各个科目推荐提分析
+  //需求二： 每个热门食品类别推荐前十个餐厅
   /*
    找到前20热点订单对应的推荐餐厅，然后找到推荐餐厅对应的用户，并统计每个用户分别包含推荐餐厅数量
-   科目  推荐题数量
-   数学   30
-   语文   15
-   英语   10
-   */
-  def hotrestaurantRecommendTop20(allInfoDS: Dataset[OrderWithRecommendations]): Unit = {
 
-    //3.1统计前20个热点餐厅，根据数量进行降序
-    val hotTop20 = allInfoDS.groupBy('food_category_id)
+   */
+  def hotfoodcategoryRecommendTop10(allInfoDS: Dataset[OrderWithRecommendations]): Unit = {
+
+    //3.1统计个热门菜品类别，根据数量进行降序
+    val hotTop20 = allInfoDS
+      .sort('score
+    ).groupBy('food_category_id)
       .agg(count("*") as "hot")
       .orderBy('hot.desc)
-      .limit(20)
+//      .limit(20)
 
-    //3.2将结果和allInfoDS进行关联，得到热点题的推荐列表
-    val ridsDF = hotTop20.join(allInfoDS, "food_category_id").select("recommendations")
-    ridsDF.show(false)
+
+    //3.2将结果和allInfoDS进行关联，得到热点餐厅的推荐列表
+    val ridsDF = hotTop20.join(allInfoDS, "food_category_id").select("food_category_id","recommendations")
+//    ridsDF.show(false)
     //3.3将获得到的推荐列表（ridsDF），转换成数据
     /*
     推荐列表：“餐厅ID_2908,餐厅ID_3162,餐厅ID_2372,餐厅ID_644,餐厅ID_206,餐厅ID_3185,餐厅ID_2895,餐厅ID_402,餐厅ID_1338,餐厅ID_3124,餐厅ID_2546,餐厅ID_999,餐厅ID_1229,餐厅ID_2862,餐厅ID_616,餐厅ID_2350,餐厅ID_1843,餐厅ID_783,餐厅ID_1027,餐厅ID_1274”
      Split(",")  切割之后去重
      */
-    val ridsDS = ridsDF.select(explode(split('recommendations, ",")) as "restaurant_id").dropDuplicates("restaurant_id")
-//    val ridsDS = ridsDF.select(explode(split('recommendations, ",")) as "restaurant_id")
-    ridsDS.show(false)
-    //3.4将 ridsDS 和 allInfoDS进行关联，得到每个推荐题目所属的科目
+//    val ridsDS = ridsDF.select(explode(split('recommendations, ",")) as "restaurant_id").dropDuplicates("restaurant_id")
+    val ridsDS = ridsDF.select(explode(split('recommendations, ",")) as "restaurant_id")
+//    ridsDS.show(false)
+    //3.4将 ridsDS 和 allInfoDS进行关联，得到每个推荐餐厅所属的菜品
     val ridAndSid = ridsDS.join(allInfoDS.dropDuplicates("restaurant_id"), "restaurant_id")
     ridAndSid.show(false)
     //3.5统计各个科目包含的推荐题目数量，并降序排序
-    val res = ridAndSid.groupBy('food_category_id)
-      .agg(count("*") as "rcount")
+    val res = ridAndSid.groupBy('restaurant_id)
+      .agg(count("recommendations") as "rcount")
       .orderBy('rcount.desc)
     //3.6输出
-    res.show()
+    println("---------每个热门食品类别推荐前十个餐厅------------")
+    res.show(10)
   }
 
+  //需求三：统计热门菜品Top10
+  def hotfoodcategoryTop10(allInfoDS: Dataset[OrderWithRecommendations]): Unit = {
+    val foodhotTop10 = allInfoDS.groupBy('food_category_id)
+      .agg(count("*")as 'hot)
+      .orderBy('hot.desc)
+    println("---------统计热门菜品Top10------------")
+    foodhotTop10.show()
+  }
+
+  //需求四：热门餐厅
+  def hotrestaurantTop10(allInfoDS: Dataset[OrderWithRecommendations]): Unit = {
+    val hotrestaurantTop10 = allInfoDS.groupBy('restaurant_id)
+      .agg(count("*") as 'hot)
+      .orderBy('hot.desc)
+    println("---------统计热门餐厅Top10------------")
+    hotrestaurantTop10.show(10)
+  }
 }
