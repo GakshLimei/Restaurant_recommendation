d90bb868aa4860c5457c68f8cc32e25dd4308db3
GakshLimei <1260671954@qq.com>
2023-05-26 03:39:27 +0800

    离线推荐

--- src/main/java/org/niit/controller/BatchDataAppController.scala
+++ src/main/java/org/niit/controller/BatchDataAppController.scala
@@ -6,7 +6,7 @@ import org.niit.service.BatchDataService
 /**
  * @author: Gary Chen
  * @Created: 2023/5/11 11:02
- * @desc:离线分析控制层
+ * @desc:离线服务控制层
  */
 class BatchDataAppController {
   //  离线分析
@@ -15,6 +15,7 @@ class BatchDataAppController {
   def dispatch(): Unit = {
 
     batchDataService.dataAnalysis()
+    batchDataService.recommendService()
 
 
   }
